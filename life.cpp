9c9ef263d8efbc5c15b428b9569cfa32f59e51f1
George Carder <george@archon.cloud>
2019-08-13 15:11:04 -0700

    housekeeping

--- life.cpp
+++ life.cpp
@@ -1,6 +1,7 @@
 #include "life.hpp"
 
 #define TEST_DIAMETER 100 // CANT HANDLE 10000, 100 is very fast
+typedef life::Universe Universe;
 
 int main(int argc, char **argv) {
   int diameter = TEST_DIAMETER;
--- life.hpp
+++ life.hpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <vector>
 
+namespace life {
+
 int saferMod(const int integer, const int modulus) {
   if (modulus < 2) return 0; 
   if (integer < 0) return modulus + integer;
@@ -62,7 +64,8 @@ struct initLivingPkg {
   int i_rowNum = -1;
   Universe *i_universe = NULL;
   std::vector<Cell*> *i_subActiveVector;
-  initLivingPkg(const int rowNum, Universe *universe, std::vector<Cell*> *subActiveVector) {
+  initLivingPkg(const int rowNum, Universe *universe, std::vector<Cell*> *subActiveVector)
+  {
     i_rowNum = rowNum;
     i_universe = universe; 
     i_subActiveVector = subActiveVector;
@@ -102,7 +105,10 @@ class Universe {
     for (int i = 0; i < diameter; ++i) {
       std::vector<Cell*> *essentialCurve = new std::vector<Cell*>;
       i_space->push_back(essentialCurve);
-      int threadRet = pthread_create(&threadId[i], NULL, initializeCurve, (void*)(new initCurvePkg(diameter, essentialCurve)));
+      int threadRet = pthread_create(&threadId[i], 
+                                     NULL, 
+                                     initializeCurve, 
+                                     (void*)(new initCurvePkg(diameter, essentialCurve)));
     } 
     for (int i = 0; i < diameter; ++i) {
       pthread_join(threadId[i], NULL); 
@@ -110,7 +116,10 @@ class Universe {
     // initialize cells
     printf("initialize cells\n"); 
     for (int i = 0; i < diameter; ++i) {
-      int threadRet = pthread_create(&threadId[i], NULL, initializeCells, (void*)(new initCellsPkg(i, i_space)));
+      int threadRet = pthread_create(&threadId[i], 
+                                     NULL, 
+                                     initializeCells, 
+                                     (void*)(new initCellsPkg(i, i_space)));
     }
     for (int i = 0; i < diameter; ++i) {
       pthread_join(threadId[i], NULL); 
@@ -120,7 +129,12 @@ class Universe {
     std::vector<std::vector<Cell*>*> subLivingVectors;
     for (int i = 0; i < diameter; ++i) {
       subLivingVectors.push_back(new std::vector<Cell*>);
-      int threadRet = pthread_create(&threadId[i], NULL, initializeLiving, (void*)(new initLivingPkg(i, this, subLivingVectors.at(i))));
+      int threadRet = pthread_create(&threadId[i], 
+                                     NULL, 
+                                     initializeLiving, 
+                                     (void*)(new initLivingPkg(i, 
+                                                               this, 
+                                                               subLivingVectors.at(i))));
     }
     for (int i = 0; i < diameter; ++i) {
       pthread_join(threadId[i], NULL); 
@@ -131,33 +145,34 @@ class Universe {
       } 
     }
     std::cout << "There are " << i_activeVector->size() << " active cells.\n";
-    long double ratio = (long double)i_activeVector->size() / (long double)(diameter * diameter);
+    long double ratio = (long double)i_activeVector->size() 
+                        / (long double)(diameter * diameter);
     std::cout << "The density is " << ratio << "\n"; 
-    
   }
 
   void startTime() {
     for(;;) {
-      
       moment();
       std::cout << testCounter << "\n";
       ++testCounter;
       usleep(1000000); 
-    
     } 
   }
 
   void moment() {
     std::cout << "For each active cell, do thing\n";
     std::vector<Cell*> activeVector = *i_activeVector; 
-    pthread_t doThreadId[activeVector.size()];
+    pthread_t threadId[activeVector.size()];
     for (int i = 0; i < activeVector.size(); ++i) {
       // cell do thing 
-      int threadRet = pthread_create(&doThreadId[i], NULL, Cell::testCellOperation, NULL);
+      int threadRet = pthread_create(&threadId[i], 
+                                     NULL, 
+                                     Cell::testCellOperation, 
+                                     NULL);
     } 
   
     for (int i = 0; i < activeVector.size(); ++i) {
-      pthread_join(doThreadId[i], NULL); 
+      pthread_join(threadId[i], NULL); 
     }
     printf("end of moment\n"); 
   }
@@ -179,10 +194,10 @@ class Universe {
     for (int i = 0; i < diameter; ++i) {
       for (int j = 0; j < 3; ++j) {
         for (int k = 0; k < 3; ++k) {
-          std::array<int, 2> coord = {saferMod(i + rays.at(j), diameter), saferMod(rowNum + rays.at(k), diameter)};
+          std::array<int, 2> coord = {saferMod(i + rays.at(j), diameter), 
+                                      saferMod(rowNum + rays.at(k), diameter)};
           Cell *neighbor = cellPkg->i_space->at(coord.at(0))->at(coord.at(1)); 
           cellPkg->i_space->at(rowNum)->at(i)->setNeighbor(neighbor);
-       
         } 
       }
     } 
@@ -207,8 +222,8 @@ class Universe {
           universe->i_space->at(rowNum)->at(i)->setIncludedInActiveVector(true); 
           pthread_mutex_unlock(&mutex_cellLock);
         } 
-
-        std::vector<Cell*> nbrsNotIncluded = universe->i_space->at(rowNum)->at(i)->getNeighborsNotInActiveVector();
+        std::vector<Cell*> nbrsNotIncluded = universe->i_space->at(rowNum)->at(i)
+                                             ->getNeighborsNotInActiveVector();
         for (Cell* n : nbrsNotIncluded) {
           if (!n->includedInActiveVector()) { 
             pthread_mutex_lock(&mutex_cellLock);
@@ -217,7 +232,6 @@ class Universe {
             pthread_mutex_unlock(&mutex_cellLock);
           }
         }            
-      
       } 
     }
     void *trivial;
@@ -225,5 +239,5 @@ class Universe {
   }
 
 };
-
+}; // namespace life
 #endif
